430f983d24dd1a0429e59caa12d43fa18d184c97
zuphzuph <zuphzuph@users.noreply.github.com>
2018-06-18 17:13:47 -0600

    Checks Registry for .NET 4.7.2

---
+++ Check_dotNET_4_7_2.ps1
@@ -0,0 +1,8 @@
+﻿$dotNET = @(Get-ChildItem "HKLM:SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\" | Get-ItemPropertyValue -Name Release | ForEach-Object { $_ -ge 461814 })
+$critical = 2
+if ($dotNET -Match "True") {
+  write-host "Version is Installed." 
+} else {
+  write-host "Version Not Installed."
+  exit $critical
+}
